1fef6720875ac7fb376245307b6204008fe6aa70
Carlos Medina <carlosmbinf@gmail.com>
2021-03-08 15:05:25 -0500

    imprime puerto saliente

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const proxyPort = 80
 proxy(proxyPort)
 .then(()=>{
   // Use it for a while....
+  console.log("Puerto iniciado por: "+ proxyPort );
 })
 .then((server) => {
   // console.log(server);
