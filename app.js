ed48f7f56cafcbe3cbdb9ccdf2b64cbdbe0d24a0
Carlos Medina <carlosmbinf@gmail.com>
2021-03-09 10:21:39 -0500

    remove port 80

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const proxy = require('@ucipass/proxy')
-const proxyPort = 80
-proxy(proxyPort)
-.then(()=>{
-  // Use it for a while....
-  console.log("Puerto iniciado por: "+ proxyPort );
-})
-.then((server) => {
-  // console.log(server);
-  // server.stop() 
-})
+// const proxy = require('@ucipass/proxy')
+// const proxyPort = 80
+// proxy(proxyPort)
+// .then(()=>{
+//   // Use it for a while....
+//   console.log("Puerto iniciado por: "+ proxyPort );
+// })
+// .then((server) => {
+//   // console.log(server);
+//   // server.stop() 
+// })
 
 var fs = require("fs");
 var appRoot = require("app-root-path");
+var httpProxy = require('http-proxy');
+
 var PATH_TO_KEY = appRoot.path + '/conf/28459803_srv5119-206152.vps.etecsa.cu.key';
  var PATH_TO_CERT = appRoot.path + '/conf/28459803_srv5119-206152.vps.etecsa.cu.cert';
- var httpProxy = require('http-proxy');
+
 var options = {
  ssl: {
  key: fs.readFileSync(PATH_TO_KEY, 'utf8'),
